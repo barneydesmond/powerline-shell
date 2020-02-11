90746bce0a1dd24dda27e965bc1f17dbbeb7f374
Barney Desmond <barneyd@arista.com>
2020-02-11 17:32:14 +1100

    Docs on usage of kubectl_context segment

--- powerline_shell/segments/kubectl_context.py
+++ powerline_shell/segments/kubectl_context.py
@@ -16,6 +16,10 @@ class Segment(BasicSegment):
             # don't have a kubectl context
             return
 
+        # Any kubectl context that isn't marked as safe in your segment config
+        # is considered as "production", which you should really be cognizant
+        # of. Can be a bit obnoxious when you always work in prod, but aeh.
+        # I keep config in ~/.powerline-shell.json for convenience.
         safe_contexts = powerline.segment_conf("kubectl_context", "safe_contexts")
         powerline.append(' %s ' % context,
                          powerline.theme.VIRTUAL_ENV_FG,
